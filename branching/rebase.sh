6d9ae48724be60831c940d7fd47de480be2010e9
Ivan.Dubskiy <niks@DESKTOP-MUP4TPH.localdomain>
2022-01-25 22:00:19 +0500

    git-rebase 2

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -3,6 +3,10 @@ cat rebase.sh
 # display command line options
 count=1
 for param in "$@"; do
+<<<<<<< HEAD
     echo "\$@ Parameter #$count = $param"
+=======
+    echo "Next parameter: $param"
+>>>>>>> 835ce5d... git-rebase 2
     count=$(( $count + 1 ))
 done
